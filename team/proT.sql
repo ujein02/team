af35a761cf9a79cb8f6a4427a1cb9f7ad2b4800e
ujein02 <ujein02@gmail.com>
2022-12-22 01:03:32 +0900

    1221 update file

--- team/proT.sql
+++ team/proT.sql
@@ -1,12 +1,12 @@
 create table USER_TB(userId varchar2(20) primary key,
 userPw varchar2(300) not null,
 userName varchar2(50),
-point number default 0,
-grade varchar2(50),
-visited int default 0,
-tel varchar2(20) not null,
-address varchar2(200),
 email varchar2(100) not null,
-birth date,
-regdate date default sysdate
+tel varchar2(20) not null,
+addr1 varchar2(200),
+addr2 varchar2(200),
+postcode varchar2(10),
+regdate date default sysdate,
+pt int default 100,
+visited int default 0
 );
